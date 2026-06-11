cc4add16ae15f753f0a5394f393ffbdbc3d8173a
BarathKrishnamoorthy <barathkrishnamoorthy17@gmail.com>
2026-06-11 16:14:20 +0530

    test1

--- app/Http/Controllers/EntryProcessController.php
+++ app/Http/Controllers/EntryProcessController.php
@@ -10584,7 +10584,7 @@ $filteredEntries = $entryProcessData->filter(function ($item) use ($emp_id, $emp
             'inhouseExternal' => $this->inhouseExternal($request, $fromDate, $toDate)->getData(true),
             'monthWiseTable' => $this->monthWiseTable($position, $fromDate, $toDate),
             // 'monthWiseTable' => $this->monthWiseTable($position, $selectedMonth),
-            'test' => "test"
+            'test1' => "test1"
 
         ]);
     }
