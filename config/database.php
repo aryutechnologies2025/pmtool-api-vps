c9530bed2e693e8e93e976a05e9dff9cf824b236
BarathKrishnamoorthy <barathkrishnamoorthy17@gmail.com>
2026-06-24 14:53:58 +0530

    Change database port from 3307 to 3306

--- config/database.php
+++ config/database.php
@@ -136,7 +136,7 @@ return [
             'write' => [
                 'host' => ['127.0.0.1'],
             ],
-            'port' => '3307',
+            'port' => '3306',
             'database' => 'medicshrms',
             'username' => 'medicsapp',
             'password' => 'YUra@2025',
